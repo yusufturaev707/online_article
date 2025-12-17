535e55475a7b8261e648c5d72e9bbe2537f6ebc0
y.to'rayev <yusufturaev707@gmail.com>
2025-12-17 20:17:17 +0500

    settings

--- config/settings.py
+++ config/settings.py
@@ -17,19 +17,20 @@ ALLOWED_HOSTS = ['*']
 # CSRF_TRUSTED_ORIGINS = ["https://ejournal.uzbmb.uz", "https://www.ejournal.uzbmb.uz"]
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# SESSION_COOKIE_AGE = 1800
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
-# #CSRF_COOKIE_HTTPONLY = True
-# SESSION_ENGINE = 'django.contrib.sessions.backends.db'
-#
-# SECURE_BROWSER_XSS_FILTER = True
-# SECURE_CONTENT_TYPE_NOSNIFF = True
-# X_FRAME_OPTIONS = 'DENY'  # iframe orqali ochishga ruxsat berilmaydi
-# SECURE_HSTS_SECONDS = 31536000  # 1 yil
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_HSTS_PRELOAD = True
-# SECURE_SSL_REDIRECT = True  # Agar HTTPS ishlatilsa
+SESSION_COOKIE_AGE = 86400
+SESSION_SAVE_EVERY_REQUEST = True
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SAMESITE = 'Lax'
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'  # iframe orqali ochishga ruxsat berilmaydi
+SECURE_HSTS_SECONDS = 31536000  # 1 yil
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_SSL_REDIRECT = True  # Agar HTTPS ishlatilsa
 
 INSTALLED_APPS = [
     'modeltranslation',
